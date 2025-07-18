7133883c79ecfbc55fe5cee40e6cc991553842c9
Lakshmiredddy <awsd851@gmail.com>
2025-07-18 09:11:23 +0530

    Removed VPC module folder as it was moved to the correct repo

--- terraform-aws-vpc/locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  common_tags={
-    project=var.project
-    environment=var.environment
-    terraform = "true"
-  }
-  az_names=slice(data.aws_availability_zones.available.names,0,2)
-}
